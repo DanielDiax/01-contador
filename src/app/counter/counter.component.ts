b284d6c9394fa9988dc84051e52a81561588fedb
unknown <jdanieldiax@gmail.com>
2022-07-25 22:07:12 -0500

    hasta las pruebas con karma

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+})
+export class CounterComponent {
+  contador: number = 0;
+
+  handleCount(value: number) {
+    //value ++
+    this.contador = value;
+  }
+
+  handleDis(value: number){
+    this.contador = value;
+  }
+}
