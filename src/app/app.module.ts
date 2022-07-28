b9d87c90b398c990e4d03bb68634d910003a1cce
unknown <jdanieldiax@gmail.com>
2022-07-27 19:57:02 -0500

    Se despliega app en netlify y comienzo a estudiar pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,13 @@ import { CounterModule } from './counter/counter.module';
   ],
   imports: [
     BrowserModule,
-    CounterModule
+    CounterModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
